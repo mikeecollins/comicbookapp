49c94fd00f20a91f46a32c7e0346e30fd9ecc26f
Your Name <you@example.com>
2023-02-24 15:02:30 +0000

    Changed Layout of project and added some event handling

--- app/src/main/java/ie/setu/comicbookapp/ComicbookActivity.kt
+++ app/src/main/java/ie/setu/comicbookapp/ComicbookActivity.kt
@@ -2,16 +2,36 @@ package ie.setu.comicbookapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import ie.setu.comicbookapp.databinding.ActivityComicbookBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 class ComicbookActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityComicbookBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_comicbook)
+
+        binding = ActivityComicbookBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
 
         Timber.plant(Timber.DebugTree())
 
         i(" Comic Book has been started..")
+
+        binding.btnAdd.setOnClickListener() {
+            val comicbookTitle = binding.comicbookTitle.text.toString()
+            if (comicbookTitle.isNotEmpty()) {
+                i("add Button Pressed: $comicbookTitle")
+            }
+            else {
+                Snackbar
+                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
     }
 }
